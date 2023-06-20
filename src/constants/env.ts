cafd6528d2bfa2314fec651f5864c437af2f1a68
mfer725 <cxw.crypto@gmail.com>
2023-06-20 21:11:05 +0800

    fix: host

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,8 @@
 export const isBuilding = typeof window === 'undefined';
-export const isProd = !isBuilding && window.location.host === 'www.themoment3.ai';
+
+export const isProd =
+  !isBuilding && (window.location.host === 'themoment3.ai' || window.location.host === 'www.themoment3.ai');
+
 export const isDev = !isProd;
 
 // export const HTTP_API_HOST = isProd ? 'https://api-web3.aliexpress.com' : 'https://pre-api-web3.aliexpress.com';
