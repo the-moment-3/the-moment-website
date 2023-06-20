530a352fe9e886f02f6ac7b478bdb547af077af1
Li,Ke <leecoor97@gmail.com>
2023-06-20 23:21:09 +0800

    feat: add transfer page and link to rules from step one's question mark

---
+++ src/pages/transfer.tsx
@@ -0,0 +1,19 @@
+import store from '@/store';
+import { LANGUAGES } from '@/constants/i18n';
+
+import styles from './transfer.module.css';
+
+export default () => {
+  const [i18n] = store.useModel('i18n');
+
+  const imgSrc = {
+    [LANGUAGES.EN]: 'https://img.alicdn.com/imgextra/i4/O1CN01ppk1bs1FlPZqayNXn_!!6000000000527-2-tps-988-597.png',
+    [LANGUAGES.KO]: 'https://img.alicdn.com/imgextra/i1/O1CN01RD6ikS1qMLgetqCDN_!!6000000005481-2-tps-988-597.png',
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <img src={imgSrc[i18n.lang]} />
+    </div>
+  );
+};
