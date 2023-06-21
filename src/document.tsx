0776e87afcfd82b784cea7fa456b16e8563da4b6
Li,Ke <leecoor97@gmail.com>
2023-06-21 11:13:56 +0800

    feat: update document

--- src/document.tsx
+++ src/document.tsx
@@ -37,7 +37,7 @@ export default () => (
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       <meta name="aplus-core" content="aplus.js" />
-      <meta name="aplus-rhost-g" content="sg.mmstat.com/alicloud" />
+      <meta name="aplus-rhost-g" content="sg.mmstat.com/" />
       <Meta />
       <Title />
       <Links />
