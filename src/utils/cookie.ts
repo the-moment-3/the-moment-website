0afa74f196e2208e8ea758be3ff0e80523525a84
mfer725 <cxw.crypto@gmail.com>
2023-06-22 14:38:52 +0800

    fix: cookie key

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,11 +1,11 @@
 // 判断浏览器是否启用 cookie
 export function isCookieEnabled() {
   // 尝试在 cookie 中设置一个值
-  document.cookie = 'testcookie=1; SameSite=Strict;';
+  document.cookie = 'test_cookie_enabled=1; SameSite=None; Secure';
   // 检查是否已设置 cookie 的值
-  const isEnabled = document.cookie.indexOf('testcookie=') !== -1;
+  const isEnabled = document.cookie.indexOf('test_cookie_enabled=') !== -1;
   // 删除测试 cookie
-  document.cookie = 'testcookie=1; SameSite=Strict; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+  document.cookie = 'test_cookie_enabled=1; SameSite=None; Secure; expires=Thu, 01 Jan 1970 00:00:00 UTC';
   // 返回是否启用 cookie 的结果
   return isEnabled;
 }
