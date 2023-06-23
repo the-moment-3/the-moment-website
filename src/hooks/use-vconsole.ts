06632316946a39d82cf2f53d09dd66057fa02b45
mfer725 <cxw.crypto@gmail.com>
2023-06-23 17:40:10 +0800

    fix: rewrites source

--- src/hooks/use-vconsole.ts
+++ src/hooks/use-vconsole.ts
@@ -12,7 +12,7 @@ const egg = `
 | |  | |  | |\\ \\      / /| |  | |  | || |         | |   \\ \\| |     | |              / |
 | |  | |  | | \\ ˉˉˉˉˉˉ / | |  | |  | ||  ˉˉˉˉˉˉˉˉ|| |    \\ ˉ |     | |     |ˉˉˉˉˉˉˉˉ  /
 ˉˉˉ  ˉˉˉ  ˉˉˉ  ˉˉˉˉˉˉˉˉ  ˉˉˉ  ˉˉˉ  ˉˉˉ ˉˉˉˉˉˉˉˉˉˉ  ˉ      ˉˉˉ      ˉˉˉ      ˉˉˉˉˉˉˉˉˉˉ
-Every Moment is Non-Fungible. ^_^ ...
+Every Moment is Non-Fungible.
 `;
 
 export function useVConsole() {
