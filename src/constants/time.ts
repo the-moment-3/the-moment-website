e56e7804741cfcf3fc9d8937a42a99bbd50417f4
Kevin <leecoor97@gmail.com>
2023-07-13 10:54:41 +0800

    fix: use BE time

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -4,10 +4,4 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-export const TASK_START_TIME = dayjs('2023-06-25T00:00').valueOf();
-export const TASK_END_TIME = dayjs('2023-07-6T00:00').valueOf();
-export const LOTTERY_TIME = dayjs('2023-07-6T12:00').valueOf();
-export const FREE_MINT_START_TIME = dayjs('2023-07-6T14:00').valueOf();
-export const FREE_MINT_END_TIME = dayjs('2023-07-13T14:00').valueOf();
-
 export const NOW = dayjs();
